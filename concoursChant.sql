7712741d1409dd7d8c3e5b2d90cb623ebdaf27f1
pierre <py.castelleta@gmail.com>
2021-11-30 16:44:01 +0100

    grid+bdd

--- concoursChant.sql
+++ concoursChant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mar. 16 nov. 2021 à 08:44
+-- Généré le : mar. 30 nov. 2021 à 16:42
 -- Version du serveur :  10.3.31-MariaDB-0ubuntu0.20.04.1
 -- Version de PHP : 7.4.3
 
@@ -30,18 +30,25 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `participant` (
   `userID` int(11) DEFAULT NULL,
-  `chanson` varchar(100) DEFAULT NULL
+  `chanson` varchar(100) NOT NULL,
+  `auteur` varchar(50) NOT NULL,
+  `bandeSon` tinyint(1) NOT NULL DEFAULT 0,
+  `validation` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `participant`
 --
 
-INSERT INTO `participant` (`userID`, `chanson`) VALUES
-(1, 'vivo per lei'),
-(3, 'la boheme'),
-(2, 'les 10 commandements'),
-(4, 'no woman no cry');
+INSERT INTO `participant` (`userID`, `chanson`, `auteur`, `bandeSon`, `validation`) VALUES
+(25, '789', '159', 0, 0),
+(5, 'Anissa', 'WEJDENE', 0, 0),
+(26, 'gabrielllle', 'Anissa', 0, 0),
+(3, 'la boheme', 'aznavour', 0, 0),
+(28, 'La danse des canards', 'Henry Dès', 0, 0),
+(2, 'les 10 commandements', 'comédie musicale', 0, 0),
+(4, 'no woman no cry', 'reobet marlet', 0, 0),
+(1, 'vivo per lei', 'bocelli', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -53,6 +60,7 @@ CREATE TABLE `users` (
   `nom` varchar(100) NOT NULL,
   `prenom` varchar(100) NOT NULL,
   `dateNaissance` date NOT NULL,
+  `numTelephone` varchar(14) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `ID` int(11) NOT NULL
@@ -62,11 +70,49 @@ CREATE TABLE `users` (
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`nom`, `prenom`, `dateNaissance`, `email`, `password`, `ID`) VALUES
-('cast', 'py', '1995-06-06', 'py.cast@gmail.com', 'cc', 1),
-('adou', 'sophie', '1995-01-01', 'sophiec@gmail.com', 'vv', 2),
-('jonathan', 'bayart', '1988-02-02', 'jo.bay@gmail.com', 'bb', 3),
-('dupont', 'bob', '2000-12-01', 'bobdup@gmail.com', 'ff', 4);
+INSERT INTO `users` (`nom`, `prenom`, `dateNaissance`, `numTelephone`, `email`, `password`, `ID`) VALUES
+('cast', 'py', '1995-06-06', NULL, 'py.cast@gmail.com', 'cc', 1),
+('adou', 'sophie', '1995-01-01', NULL, 'sophiec@gmail.com', 'vv', 2),
+('bayart', 'jonathan', '1988-02-02', NULL, 'jo.bay@gmail.com', 'bb', 3),
+('dupont', 'bob', '2000-12-01', NULL, 'bobdup@gmail.com', 'ff', 4),
+('Charles', 'Ray', '1980-01-01', NULL, 'r.charles@gmail.com', 'odd', 5),
+('Bozo', 'Jijo', '1906-06-06', NULL, 'mdr@orange.fr', 'bonjour', 6),
+('JL', 'Follet', '1960-12-02', NULL, 'j.follet@ac-lille.fr', '$2y$10$obXVYJARTz8r5XMRjMeqEuWz2oE/69ZIxTgvicJ9APo9bhqgLioSO', 9),
+('Cast', 'Py', '1555-06-06', NULL, 'helsllc@gmgmg.fr', '$2y$10$vlV4BOLOTUJx2cqxUYBcOe6bN9eQnvLVz9aQvHhys8Gw7cOztuoQe', 10),
+('Maurice', 'Joe', '1975-12-05', '0614555555', 'j.momo@orange.com', '$2y$10$UpLm5sbmKIL09WL85j.Na.7P5aLcfcaKQKCmHj8VHliOZbCJuQFfC', 11),
+('Romain', 'Christophe', '1977-02-15', '0606060606', 'cromin@hacks.net', '$2y$10$F4iAsRIdYpnvGZ.Op48XMe3zx8zcriwPTZlSBDYb2.1sbnkgJRf5C', 12),
+('tom', 'van', '1998-08-15', '0614958445', 'tomvan@cc.nom', '$2y$10$eatXDqit438aPXONqO.myesFhHhKBVeIq4isQH6KPQkFFuItx0vP6', 13),
+('tom', 'van', '1998-08-15', '0614958445', 'tomvan@ccb.nom', '$2y$10$LMHWgHTbdnB.TWCAfONkuuoqwsiRzXvaUfTZkwLbR.8WYYVVO.Swe', 14),
+('maxime', 'bayou', '1944-05-05', '0606606060', 'maxbay@orange.fr', '$2y$10$GTwaSTS203YvhGowEBpPS.9Ale90UdDjquluZqWZ/ef6jePGVKXKm', 15),
+('Bernadette', 'Chirac', '1920-02-23', '0756451234', 'berniechichi@elysee.fr', '$2y$10$PO4K99786Kt/3Dx591ApxeMB4MN4GhPru.sP62CNE8KnVLYbvJ1My', 16),
+('Mentos', 'Menthe', '1995-06-06', '0606060606', 'mlem@gmail.com', '$2y$10$4Dd5Hg45gDMCXBlCOP4psOKloYkMRj5oMgzJ/Kl0zZFWO1H1QC61C', 17),
+('Jordy', 'Del', '1930-06-06', '0606060606', 'jlej@ribot.fr', '$2y$10$L3Gf8inV8HRLLKfT5V77E.mK3V.D6vAVq5Cry48z9Mvn6bR0e34zy', 18),
+('Joe', 'Dassin', '1930-12-12', '+32612304578', 'jodasse@gmail.com', '$2y$10$z3A963GGQoy01zeOT3rvKOEVm5p3znGZgx9tkRRfwXa0XHJzCpZAG', 19),
+('Toast', 'Bread', '1999-06-06', '+32620454545', 't.t@gmail.com', '$2y$10$yHKdWr/ceps0cH4MSxHzhuV.b3vW/BMN9IwKSnGXAZtmJE/6vHXAm', 20),
+('Dave', 'Dave', '1950-02-14', '+32615151515', 'd.d@gmail.com', '$2y$10$xMaWAAjSDn9M.OIcu.CyZu/UhZAJQ0zMjzzf47HvXarGalL8QlBTa', 21),
+('Denorme', 'Florent', '1985-05-16', '+33612534595', 'd.f@orange.fr', '$2y$10$EYdpuXtuICtXSOhk8ha5VecMGXP0B3s/MqJ13dpJvWoEkqwMhJOW2', 22),
+('mike', 'mike', '1980-06-06', '06066060660660', 'm.m@gmail.com', '$2y$10$X3jvf478kVKqWTXtjk3c8eqP1v6uWdrE74VEnKqhpxWPDVhRvgheK', 23),
+('Erwan', 'Presse', '1994-09-25', '+33612564578', 'e.presse@gmail.com', '$2y$10$MvX2sIzsA4kZdM9/Q9JIj.s2ile55jUhkk5oQec1gaUl0xt6gj0X6', 24),
+('Personne', 'Test', '1920-01-01', '00000000000000', 'test@test.fr', '$2y$10$M01ZOIdo5dnF7AAej7s4oOkaKuZocLICylDaVVWclGQNL7GL69P92', 25),
+('Daniel', 'Declerck', '1970-12-01', '06060660660660', 'dan@gmail.com', '$2y$10$qVy3b4UPiov1h41nGvM0yOHCR0m9vgkFj7InkhPuI/Z8/eILRV3k2', 26),
+('Dupontel', 'Géraldine', '2010-09-05', '0715151515', 'dupontel.g@gmail.com', '$2y$10$LWuIq31NWLHj.6hrSwRwmObLsCw6Ztmal9YtpEMJjEGrGjb05smGm', 27),
+('Hinder', 'Aymeric', '1993-09-12', '0612345678', 'a.hinder@bpascal.fr', '$2y$10$T8JqNb7X9uG6kBoPCvn9auogH/CiJx1/M42SnUj9T66cWPPlJ.ZR.', 28),
+('Guillaume', 'Tourneur', '1990-06-06', '06060660606066', 'guitour@mail.fr', '$2y$10$FeKG2Myx601r6nai284XUeHCnzmlifsdFSn/yxtk7VJbmhdOZolNy', 29),
+('Joey', 'Starr', '1966-06-06', NULL, 'ptitloulou93@live.fr', '$2y$10$IabUVk6TP/OSgIUmGUoqfOuyS2ojZTrDoJ0Av0dYXQvItGS3xOJhC', 30),
+('Joey', 'Starr', '1966-06-06', NULL, 'ptitloulou931@live.fr', '$2y$10$TNLuMVS0Eg0kvbLJ04l99OVFX7Mk09zJZppJUzFqEmOUHT.fAZRqm', 31),
+('Joey', 'Star', '1966-06-06', NULL, 'ptitloulou31@live.fr', '$2y$10$FJ2wi8jPPmhRnqVg.pIHquSrfT7RAP1.d1O2Wq5VsALKJKcxUwACW', 33),
+('Joey', 'Star', '1966-06-06', NULL, 'ptitloulou3199@live.fr', '$2y$10$9VPxmjODedDyfmumrCVMtOrFBU4zc5vfLew97XcHbhOWQTZMkpTH6', 34),
+('theo', 'theo', '1950-12-01', NULL, 't.t.t@gmail.com', '$2y$10$rZBp80LlM56LMkR3mVgvwOkCnIrBb5pUXPY6GntGhaVWYSql4hy9e', 35),
+('bbj', 'jkmlk', '1972-12-12', NULL, 'tilt@yahoo.fr', '$2y$10$DgnrIzA5o0tZLq7OjM2Z3eieCPpTEFQUtWnidUwEw5TA3FaOnwOou', 36),
+('coucou', 'hibou', '2010-12-12', NULL, 'chib@foret.com', '$2y$10$a4ZeBDjD2SBrSQhyAgEQNOqw1bJoWUxk/dlAwabLg2A1W8bUWp6yG', 37),
+('brief', 'projet', '1999-12-15', NULL, 'bproj@yahoo.fr', '$2y$10$O05q35Z5Gd0.l9FYSklyJOhr2Uq59JG.fQUlT.br94GxjuDHh4C5G', 38),
+('rob', 'rob', '1999-02-02', NULL, 'r.r.r@yahoo.fr', '$2y$10$/oNANA8cjCC5/9e.wnKsJOvuZ57WlTki0M8Pmgzg8tSiw7mBvr0p6', 39),
+('Sidki', 'Mehdi', '1999-12-12', '66666666666666', 'm.s@mail.fr', '$2y$10$oxzcdqEPdLTZhWwcHmv3derjMcHNjL9Q8XWpJtVFUmXNNCgF.bANq', 40),
+('bop', 'bip', '1999-02-02', '55555555555555', 'bb.b@mail.com', '$2y$10$LvtZVOvTcv3hFaKWeMyqPOD7lmvr.U7XCsDVO8n.UqKMAf1UGCcXu', 41),
+('Regniez', 'Jeremy', '1970-05-05', '66666666666666', 'jeregn@mail.fr', '$2y$10$lJ8pC4r2eV0n8H6R58NSXunNMZflfiVUyBigjgeq9UpSKTftx1P5.', 42),
+('Aliagas', 'Nikos', '1950-01-01', '11111111111111', 'nikaliag@free.fr', '$2y$10$r/Hg16idw//1qX4BhHbNPO1aYUsoIEgIy2qUr4v6KloYjOI8qGqs6', 43),
+('Admin', 'Admin', '1920-01-01', '00000000000000', 'admin@mail.com', '$2y$10$qcezwrZdFhSmhN3UIbzTGOdpRtJUy9cYZW1MJaiLjz5pkPZKWIW62', 44),
+('Son-Forget', 'Joachim', '1990-11-11', '66666666666666', 'j.ss@mail.fr', '$2y$10$plpYrFKVREJ3s2mWVl1U2OwWgtiUfg..p3eK1tryouit4UT6GNhYO', 45);
 
 --
 -- Index pour les tables déchargées
@@ -76,8 +122,8 @@ INSERT INTO `users` (`nom`, `prenom`, `dateNaissance`, `email`, `password`, `ID`
 -- Index pour la table `participant`
 --
 ALTER TABLE `participant`
-  ADD UNIQUE KEY `userID` (`userID`),
-  ADD UNIQUE KEY `chanson` (`chanson`);
+  ADD UNIQUE KEY `chanson` (`chanson`),
+  ADD UNIQUE KEY `userID` (`userID`);
 
 --
 -- Index pour la table `users`
@@ -95,7 +141,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- Contraintes pour les tables déchargées
